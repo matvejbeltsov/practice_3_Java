5959ee208b1af654dbdc419cd04cde838779b007
MatvejBeltsov <matvej.beltsov@gmail.com>
2023-10-12 19:55:26 +0300

    Создание интерфейса MovieApi

--- src/main/java/ru/mirea/Beltsov/Films/MovieApi.java
+++ src/main/java/ru/mirea/Beltsov/Films/MovieApi.java
@@ -1,2 +1,11 @@
-package ru.mirea.Beltsov.Films;public class MovieAPI {
+package ru.mirea.Beltsov.Films;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+import java.util.List;
+
+public interface MovieApi {
+    @GET("https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json")
+    Call<List<Movie>> getMovies();
 }
